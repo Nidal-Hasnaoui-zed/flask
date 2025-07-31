e6e102be2d3ca13ed60fbedf2390029c9e7bba26
Hairo <hasnaouinidal308@gmail.com>
2025-07-31 21:02:42 +0100

    just adding other function bro !

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def say_hey(): 
     return 'hey my name is nidal hasnaoui !'
+
+#adding other function in other url !
+@app.route('/page2')
+def page2(): 
+    return '<h1>Welcome from page 2 !</h1>'
 
 if __name__ == '__main__' : 
     app.run()
