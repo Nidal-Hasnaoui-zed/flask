7ff7d5b875410eeafb26c19614dff438cdb6ed47
Hairo <hasnaouinidal308@gmail.com>
2025-07-31 13:38:43 +0100

    understanding first simple app

--- app.py
+++ app.py
@@ -6,19 +6,17 @@ from flask import Flask
 #why ? : bc if you run the file direclty , '__name__' will be '__main__'
 app = Flask(__name__)
 
+# this is the dirictor will link page (/) 'landing page' with hsi function !
+# so ? : if you opend this link will run next function !
 @app.route('/') 
-
 
 # this is a simple function return my name !
 def say_hey(): 
     return 'hey my name is nidal !'
 
-# lets add some other page !
-def job(): 
-    return 'my job is back end Dev !'
-app.add_url_rule('/job', 'job', job)   # and i dont understand this bro bc  just coppy it 
 
-#and  i dont understand this ine bro !
+
+#here you said if i run the app diriclty gives me reszult of this file !
 if __name__ == '__main__' : 
     app.run()
     
