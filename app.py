d0666caebc9cf31ccd1f6933898243db02f7175e
Hairo <hasnaouinidal308@gmail.com>
2025-08-01 18:39:25 +0100

    this is how i can handel a pamars

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def handel_parms():
 # let me show you how you can do it with better method bro !
 @app.route('/handel_parms2')
 def handel_parms2(): 
-    if 'greating' in request.get.keys() and 'name' in request.get.keys(): 
+    if 'greating' in request.args.keys() and 'name' in request.args.keys(): 
         greating  = request.args['greating']
         name = request.args.get('name')
         return f'{greating}, {name}'
