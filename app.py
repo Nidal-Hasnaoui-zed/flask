ec6ed9fdb20ba43bd6910e66dea028c7d539a30b
Hairo <hasnaouinidal308@gmail.com>
2025-08-01 18:31:57 +0100

    handel params bro !

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask , request
 
 app = Flask(__name__)
 
@@ -20,7 +20,11 @@ def greating(name):
 @app.route('/math/<int:num1>/<int:num2>')
 def get_sum(num1,num2):
     return f'the sum is {num1 + num2}' 
-    
+
+# lets handle paramets bro ! :
+@app.route('/handel_parms')
+def handel_parms(): 
+    return str(request.args)
 
 if __name__ == '__main__': 
     app.run(debug=True)
