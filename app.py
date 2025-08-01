b7d71453508e064293dc145e7567299157399384
Hairo <hasnaouinidal308@gmail.com>
2025-08-01 19:03:15 +0100

    this is how you can use python code in html file

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ def index1():
     name = 'nidal'
     age = 19 
     dream_job = 'backend Dev'
-    return render_template('index1.html', name=name, age=age, dream_job=dream_job)
+    my_langs = ['html','css','js','php','python']
+    return render_template('index1.html', name=name, age=age, dream_job=dream_job,my_langs=my_langs)
 
 if __name__ == '__main__' : 
     app.run(debug=True)
