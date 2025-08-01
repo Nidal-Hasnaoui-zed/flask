9748b4c08c42c212ba14e99616b8c1d681ff7b3a
Hairo <hasnaouinidal308@gmail.com>
2025-08-01 18:56:25 +0100

    Work with varaibales in hmtml chapter

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import Flask , render_template
 
 app = Flask(__name__, template_folder='templates')
 
+@app.route('/test')
+def index2(): 
+    return render_template('index.html')
+
 @app.route('/')
-def testing(): 
+def index1(): 
     name = 'nidal'
     age = 19 
     dream_job = 'backend Dev'
-    return render_template('index.html', name=name, age=age, dream_job=dream_job)
+    return render_template('index1.html', name=name, age=age, dream_job=dream_job)
 
 if __name__ == '__main__' : 
     app.run(debug=True)
