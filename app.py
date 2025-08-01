035b7e523c04904b0b7b91a803fccb390915ffc3
Hairo <hasnaouinidal308@gmail.com>
2025-08-01 22:43:15 +0100

    i create the function of filters !

--- app.py
+++ app.py
@@ -14,6 +14,12 @@ def index1():
     my_langs = ['html','css','js','php','python']
     return render_template('index1.html', name=name, age=age, dream_job=dream_job,my_langs=my_langs)
 
+#filters : 
+@app.route('/other')
+def other(): 
+    some_text = 'nidal'
+    return render_template('other.html', some_text=some_text)
+
 if __name__ == '__main__' : 
     app.run(debug=True)
     
