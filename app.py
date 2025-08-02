007ea3af40444ad38ae3e225fe36d5acdb4556d6
Hairo <hasnaouinidal308@gmail.com>
2025-08-02 21:09:14 +0100

    learn how you can work with dynamic variables in flask !

--- app.py
+++ app.py
@@ -7,5 +7,16 @@ def Hey():
     return render_template('index.html')
 
 
+#dynmic valirables : 
+@app.route('/index1')
+def index1(): 
+    name = 'nidal'
+    lname = 'hasnaoui'
+    age = 19 
+    job = 'backend'
+    langs = ['js','php','pyhton','sql']
+    return render_template('index1.html', name=name , lname=lname, age=age, job=job,langs=langs)
+    
+
 if __name__ == '__main__': 
     app.run(debug=True)
