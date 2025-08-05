cd9e7731e66931562897acb33229f58e3733e8e0
Hairo <hasnaouinidal308@gmail.com>
2025-08-05 18:33:48 +0100

    this if tne method is get i will show the page !

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 from flask import Flask , template_rendered , request
 
 app = Flask(__name__, template_folder='templates')
+
+@app.rout('/', methods=['GET','POST'])
+def index(): 
+    if request.method == 'GET' : 
+        return template_rendered('index.html')
