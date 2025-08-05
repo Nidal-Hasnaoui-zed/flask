84ed98df64bddeb0e16949414c735d4404a081bb
Hairo <hasnaouinidal308@gmail.com>
2025-08-05 21:55:32 +0100

    understanding the fucking static files !

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask , template_rendered 
+from flask import Flask ,render_template
 
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
 
 @app.route('/') 
 def index() : 
-    return template_rendered('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__' : 
     app.run(debug=True) 
