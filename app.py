7acf611439d7cb896a0bb9c5bdb5a4f122864419
Hairo <hasnaouinidal308@gmail.com>
2025-08-05 19:06:27 +0100

    makign the app run bro

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ from flask import Flask , template_rendered
 
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
 
+@app.route('/') 
+def index() : 
+    return template_rendered('index.html')
+
+if __name__ == '__main__' : 
+    app.run(debug=True) 
