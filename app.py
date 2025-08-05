8bd8620052c764b843129534b36b43dcf0b22992
Hairo <hasnaouinidal308@gmail.com>
2025-08-05 18:35:15 +0100

    this for if the method is post i will handle it !

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ app = Flask(__name__, template_folder='templates')
 def index(): 
     if request.method == 'GET' : 
         return template_rendered('index.html')
+    elif request.method == 'POST' : 
+        user_name = request.form.get('username')
+        password = request.form.get('pwd')
