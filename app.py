2ae3af4779d9ef76ec0762c274564819fbf8c7aa
Hairo <hasnaouinidal308@gmail.com>
2025-08-05 13:40:53 +0100

    this how you can handle a simple form on flask !

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index(): 
-    return render_template('index.html')
-
+    if request.method == 'GET' : 
+        return render_template('index.html')
+    elif request.method == 'POST' : 
+        username = request.form.get('username')
+        pwd = request.form.get('pwd')
+        
+        if username == 'nidal' and pwd == 'nidal' : 
+            return 'welcome !'
+        else : 
+            return 'Sorry Try Again !'
+ 
 if __name__ == '__main__': 
     app.run(debug=True)
