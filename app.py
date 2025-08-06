dfb740f6fff755ac15862b6af6297b5808e312de
Hairo <hasnaouinidal308@gmail.com>
2025-08-06 22:39:11 +0100

    making the fukcinf nidal name!

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , session , make_response
 
 app = Flask(__name__ ,template_folder='templates')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', message='index')
+
  
 if __name__ == '__main__'  : 
     app.run(debug=True)
