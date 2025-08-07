444d62c4967584b21acfd5e8548e5fd5bfac5129
Hairo <hasnaouinidal308@gmail.com>
2025-08-07 17:26:38 +0100

    hey this me bro and this hou you can get and remove a cookie !

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , session, make_response
+from flask import Flask , render_template , session, make_response, request
 
 app = Flask(__name__ , template_folder='templates')
 
@@ -38,7 +38,26 @@ def set_cookie():
     response = make_response(render_template('index.html', message='creating a cookie'))
     response.set_cookie('Cookie_name', 'Cookie_value'); 
     return response
-   
+
+#get a cookie ! 
+@app.route('/get_cookie')
+def get_cookie(): 
+    cookie_value = request.cookies['Cookie_name']
+    return render_template('index.html', message=f'the cookie value is {cookie_value}')
+
+
+# remove a cookie !
+@app.route('/remove_cookie')
+def remove_cookie():
+    response = make_response(render_template('index.html', message='Cookie removed.'))
+    response.set_cookie('cookie_name', '', expires=0)
+    return response
+
+
+# create a login form !
+@app.route('/login')
+def Login(): 
+    
    
    
 if __name__ == '__main__': 
