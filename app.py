3cf22dfff758b6c8d0be1e99d883d63bed8e1afa
Hairo <hasnaouinidal308@gmail.com>
2025-08-07 17:07:55 +0100

    set a session !

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template 
+from flask import Flask , render_template , session
 
 app = Flask(__name__ , template_folder='templates')
 
@@ -8,5 +8,14 @@ app.secret_key = 'nidal'
 def index(): 
     return render_template('index.html', message='this is the index page !')
 
+# we will set an session !
+@app.route('/set_data')
+def set_data(): 
+    session['username'] = 'nidal'
+    session['job'] = 'backend'
+    return render_template('index.html', message='this is how  you can set data in session !')
+
+
+ 
 if __name__ == '__main__': 
     app.run(debug=True)
