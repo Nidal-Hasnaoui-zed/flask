a8514d1055aad652fe8007909f28a45ae049189a
Hairo <hasnaouinidal308@gmail.com>
2025-08-07 17:38:39 +0100

    finisihf flaks from this day 1

--- app.py
+++ app.py
@@ -1,64 +1,72 @@
-from flask import Flask , render_template , session, make_response, request
+from flask import Flask, render_template, session, make_response, request, flash
 
-app = Flask(__name__ , template_folder='templates')
+app = Flask(__name__, template_folder='templates')
+app.secret_key = 'nidal'  # ضروري للـ session و flash
 
-app.secret_key = 'nidal'
-
+# صفحة البداية
 @app.route('/')
 def index(): 
-    return render_template('index.html', message='this is the index page !')
+    return render_template('index.html', message='This is the index page!')
 
-# we will set an session !
+# حفظ بيانات في الـ session
 @app.route('/set_data')
 def set_data(): 
     session['username'] = 'nidal'
     session['job'] = 'backend'
-    return render_template('index.html', message='this is how  you can set data in session !')
+    return render_template('index.html', message='Data has been set in session!')
 
-
-#lets get a data from a session !
+# جلب البيانات من الـ session
 @app.route('/get_data')
-def get_data() : 
-    if 'usernmae' in session.keys() and 'job' in session.keys() : 
-        username = session['usename']
+def get_data(): 
+    if 'username' in session and 'job' in session: 
+        username = session['username']
         job = session['job']
-        return render_template('index.html', mesage=f'the user name is {username} and job is {job}')
-    else : 
-        return render_template('index.html', message='no session found !')
-    
-#clear session !
-@app.route('/clear_session')
-def session_clear() : 
-    session.clear()
-    render_template('index.html', message='all session was cleard !')
+        return render_template('index.html', message=f'The user name is {username} and job is {job}')
+    else: 
+        return render_template('index.html', message='No session found!')
 
-#set a coookie !
-@app.route('/set_cookie ')
+# مسح الـ session
+@app.route('/clear_session')
+def session_clear(): 
+    session.clear()
+    return render_template('index.html', message='All session data was cleared!')
+
+# إنشاء كوكي
+@app.route('/set_cookie')
 def set_cookie(): 
-    response = make_response(render_template('index.html', message='creating a cookie'))
-    response.set_cookie('Cookie_name', 'Cookie_value'); 
+    response = make_response(render_template('index.html', message='Cookie created!'))
+    response.set_cookie('cookie_name', 'cookie_value')
     return response
 
-#get a cookie ! 
+# جلب الكوكي
 @app.route('/get_cookie')
 def get_cookie(): 
-    cookie_value = request.cookies['Cookie_name']
-    return render_template('index.html', message=f'the cookie value is {cookie_value}')
+    cookie_value = request.cookies.get('cookie_name')
+    if cookie_value:
+        return render_template('index.html', message=f'The cookie value is: {cookie_value}')
+    else:
+        return render_template('index.html', message='No cookie found.')
 
-
-# remove a cookie !
+# إزالة الكوكي
 @app.route('/remove_cookie')
 def remove_cookie():
     response = make_response(render_template('index.html', message='Cookie removed.'))
     response.set_cookie('cookie_name', '', expires=0)
     return response
 
+# صفحة تسجيل الدخول
+@app.route('/login', methods=['GET', 'POST'])
+def login(): 
+    if request.method == 'GET': 
+        return render_template('index.html')
+    elif request.method == 'POST': 
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == 'nidal' and password == 'nidal': 
+            flash('Successful login!')
+        else: 
+            flash('Login failed!')
+        return render_template('index.html')
 
-# create a login form !
-@app.route('/login')
-def Login(): 
-    
-   
-   
 if __name__ == '__main__': 
     app.run(debug=True)
