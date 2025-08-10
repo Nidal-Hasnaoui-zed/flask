831f4a200d7a78df4859e410263af5dfe8a2525c
Hairo <hasnaouinidal308@gmail.com>
2025-08-10 21:19:42 +0100

    and here i undertsand the app.py file in our project !

--- db_apps/app.py
+++ db_apps/app.py
@@ -1,2 +1,17 @@
 from flask import Flask 
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+
+db = SQLAlchemy()
+
+def create_app(): 
+    app = Flask(__name__, template_folder='templates')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testdb.db'
+    
+    db.init_app(app)
+    Migrate(app , db)
+    
+    # after you create the route file imprte hem here !
+    
+    
+    return app  
