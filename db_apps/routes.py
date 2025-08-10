9c3d427f5ec598bb9b902a681477d52dec90e436
Hairo <hasnaouinidal308@gmail.com>
2025-08-10 21:39:32 +0100

    we just creating a nidal name !

---
+++ db_apps/routes.py
@@ -0,0 +1,15 @@
+from flask import render_template , request
+from models import Person 
+from app import db  
+
+
+def  register_routes(app): 
+    @app.route('/')
+    def index(): 
+        people = Person.query.all()
+        
+        if not people : 
+            return 'No data fond !' 
+        
+        return '<br>'.join([repr(p) for p in people]) 
+    
