3640fe1de4267ff862ad565b21ffb2ef581e767a
Hairo <hasnaouinidal308@gmail.com>
2025-08-10 14:57:10 +0100

    fix modles file !

--- db_apps/models.py
+++ db_apps/models.py
@@ -1,12 +1,12 @@
-from app import db 
+from app import db
 
-class Person(db.Model): 
-    __tablename__ = 'people'
-    
-    pid = db.Column(db.Intger , primary_key=True)
-    name = db.Column(db.text, nullable=False)
-    age = db.Column(db.Intger)
-    job = db.Column(db.text)
-    
-    def __repr__(self): 
-        return f'name is {self.name} and age is {self.age} and job is {self.age}'
+class Person(db.Model):
+    __tablename__ = 'people'  # table name in DB
+
+    pid = db.Column(db.Integer, primary_key=True)  # Primary key column
+    name = db.Column(db.String(100), nullable=False)  # Name is required
+    age = db.Column(db.Integer)
+    job = db.Column(db.String(100))
+
+    def __repr__(self):
+        return f"<Person id={self.pid} name='{self.name}' age={self.age} job='{self.job}'>"
