1328ae6a75275924dfc32680f4cec08d662204cb
Hairo <hasnaouinidal308@gmail.com>
2025-08-10 21:31:04 +0100

    this is the models file ! was finihsed

--- db_apps/models.py
+++ db_apps/models.py
@@ -1,1 +1,13 @@
 from app import db 
+
+class Person(db.Model) : 
+
+    __tablename__ = 'people'
+    
+    pid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer)
+    job = db.Column(db.String(100))
+    
+    def __repr__(self): 
+        return f'hey my name is {self.name} and my age is {self.age} and my job id {self.job}'
