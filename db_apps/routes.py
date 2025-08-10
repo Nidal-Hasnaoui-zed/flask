ef5282a8028847b99937c933936b0c07abb17347
Hairo <hasnaouinidal308@gmail.com>
2025-08-10 14:58:12 +0100

    fix the routes file !

--- db_apps/routes.py
+++ db_apps/routes.py
@@ -1,10 +1,25 @@
-from flask import render_template , request 
+from flask import render_template, request
+from models import Person
+from app import db
 
-from models import Person 
+def register_routes(app):
 
-def register_routes(app, db): 
-    
     @app.route('/')
-    def index(): 
-        people = Person.query.all()
-        return str(people)
+    def index():
+        people = Person.query.all()  # Get all people from DB
+        return render_template('index.html', people=people)
+
+    @app.route('/add', methods=['POST'])
+    def add_person():
+        name = request.form.get('name')
+        age = request.form.get('age')
+        job = request.form.get('job')
+
+        if not name:
+            return "Name is required!", 400
+
+        person = Person(name=name, age=int(age) if age else None, job=job)
+        db.session.add(person)
+        db.session.commit()
+
+        return f"Added {person.name} successfully!"
