4d004d129a194a5f331145294c436a6f6ddbf3fc
Hairo <hasnaouinidal308@gmail.com>
2025-08-11 21:24:56 +0100

    thsi is me and i link the index page with his endpoint !

--- db_apps/routes.py
+++ db_apps/routes.py
@@ -9,16 +9,20 @@ def register_routes(app):
         # quick plain text response (you can replace with render_template later)
         if not people:
             return "No people in DB yet"
-        return '<br>'.join([repr(p) for p in people])
+        return render_template('index.html')
 
-    @app.route('/add', methods=['POST'])
-    def add_person():
-        name = request.form.get('name')
-        age = request.form.get('age')
-        job = request.form.get('job')
-        if not name:
-            return "Name required", 400
-        p = Person(name=name, age=int(age) if age else None, job=job)
-        db.session.add(p)
-        db.session.commit()
-        return f"Added {p.name}"
+
+
+    # this is advenced level bro !
+
+    # @app.route('/add', methods=['POST'])
+    # def add_person():
+    #     name = request.form.get('name')
+    #     age = request.form.get('age')
+    #     job = request.form.get('job')
+    #     if not name:
+    #         return "Name required", 400
+    #     p = Person(name=name, age=int(age) if age else None, job=job)
+    #     db.session.add(p)
+    #     db.session.commit()
+    #     return f"Added {p.name}"
