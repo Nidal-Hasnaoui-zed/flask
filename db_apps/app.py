05269b0cea886ce15ed9cf71e6fcd394b6a97537
Hairo <hasnaouinidal308@gmail.com>
2025-08-11 17:24:54 +0100

    adding the content of app.py file !

---
+++ db_apps/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__, template_folder='templates')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testdb.db'   # relative sqlite file
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    # Import routes after db is defined to avoid circular import issues
+    from routes import register_routes
+    register_routes(app)
+
+    return app
