6f963eb2ba742c2a9df03a5984f087a5e7a7c9e9
Hairo <hasnaouinidal308@gmail.com>
2025-08-14 11:10:59 +0100

    this is me bro !

--- user_login/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, request
-from models import User
-from app import db  # import db only when needed (safe here)
-
-def register_routes(app):
-    @app.route('/', methods=['GET', 'POST'])
-    def index():
-       return ""
