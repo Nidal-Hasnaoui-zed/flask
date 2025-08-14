c4195667ca3cfabf53106ef5203670727fc0f860
Hairo <hasnaouinidal308@gmail.com>
2025-08-14 11:44:22 +0100

    connect db !

--- user_login/app.py
+++ user_login/app.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testdb.db'   # relative sqlite file
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/login_db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
--- user_login/models.py
+++ user_login/models.py
@@ -1,13 +1,17 @@
+from flask_login import UserMixin
 from app import db
 
-class Person(db.Model):
-    __tablename__ = 'people'
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
 
-    pid = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer)
-    job = db.Column(db.String(100))
+    uid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(50))
+    description = db.Column(db.String(255))
 
     def __repr__(self):
-        return f"<Person id={self.pid} name={self.name!r} age={self.age} job={self.job!r}>"
+        return f'<User: {self.username}, Role: {self.role}>'
 
+    def get_id(self):
+        return str(self.uid)  # Flask-Login expects a string ID
